bafcbc14204a18559bc6a871c44477b33beb16a4
Kogi1337 <alexkogler0@gmail.com>
2021-10-31 10:05:10 +0100

    Change - scale of iamges in sidebar

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -35,133 +35,78 @@ export default class Sidebar extends React.Component {
                 <img
                   src={Activity}
                   alt="activity_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
-                <img src={Action} alt="action_image" height={50} width={50} />
+                <img src={Action} alt="action_image" height={75} width={75} />
 
                 <img
                   src={controlFlow}
                   alt="controlFlow_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={objectFlow}
                   alt="objectFlow_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={initialNode}
                   alt="initialNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={finalNode}
                   alt="finalNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={decisionNode}
                   alt="decisionNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={mergeNode}
                   alt="mergeNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={forkNode}
                   alt="forkNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={joinNode}
                   alt="joinNode_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
 
                 <img
                   src={swimlane}
                   alt="swimlane_image"
-                  height={50}
-                  width={50}
+                  height={75}
+                  width={75}
                 />
               </div>
             </SubMenu>
           </Menu>
         </div>
-
-        {/* <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['components']}
-        >
-          <SubMenu key="components" title="Components">
-            <Menu.Item key="1" style={{ height: '100px' }}>
-              <img
-                src={Activity}
-                alt="activity_image"
-                height={80}
-                width={110}
-              />
-            </Menu.Item>
-            <Menu.Item key="2" style={{ height: '100px' }}>
-              <img src={Action} alt="action_image" height={80} width={110} />
-            </Menu.Item>
-            <Menu.Item key="3" style={{ height: '100px' }}>
-              <img
-                src={controlFlow}
-                alt="controlFlow_image"
-                height={80}
-                width={110}
-              />
-            </Menu.Item>
-            <Menu.Item key="4" style={{ height: '100px' }}>
-              Object flow
-            </Menu.Item>
-            <Menu.Item key="5" style={{ height: '100px' }}>
-              Initial node
-            </Menu.Item>
-            <Menu.Item key="6" style={{ height: '100px' }}>
-              <img
-                src={finalNode}
-                alt="finalNode_image"
-                height={80}
-                width={110}
-              />
-            </Menu.Item>
-            <Menu.Item key="7" style={{ height: '100px' }}>
-              <img
-                src={decisionNode}
-                alt="decisionNode_image"
-                height={80}
-                width={110}
-              />
-            </Menu.Item>
-            <Menu.Item key="8">Decision node</Menu.Item>
-            <Menu.Item key="9">Merge node</Menu.Item>
-            <Menu.Item key="10">Fork node</Menu.Item>
-            <Menu.Item key="11">Join node</Menu.Item>
-            <Menu.Item key="12">Swimlane and partition</Menu.Item>
-          </SubMenu>
-        </Menu> */}
       </Sider>
     );
   }
