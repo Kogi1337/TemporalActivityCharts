b4d94a03379f103f5da6b5e0ea61176edb634217
Alexander Kogler <alexkogler0@gmail.com>
2022-01-17 13:06:04 +0100

    Add - some style changes to activity and constraint edge

--- src/EdgeTypes/TimeConstraintEdgeTop.js
+++ src/EdgeTypes/TimeConstraintEdgeTop.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { InputNumber } from 'antd';
+import React from "react";
+import { InputNumber, Select } from "antd";
 
-const foreignObjectSize = 100;
+const foreignObjectSize = 140;
 
 export default function TimeConstraintEdgeTop({
   id,
@@ -13,45 +13,56 @@ export default function TimeConstraintEdgeTop({
   data,
 }) {
   const path =
-    'M' +
+    "M" +
     sourceX +
-    ' ' +
+    " " +
     sourceY +
-    'Q' +
+    "Q" +
     (sourceX + targetX) / 2 +
-    ' ' +
+    " " +
     (sourceY > targetY ? sourceY - 200 : targetY - 200) +
-    ', ' +
+    ", " +
     targetX +
-    ' ' +
+    " " +
     targetY;
+
+  const selectBefore = (
+    <Select
+      defaultValue={data?.constraintType}
+      onChange={(value) => (data.constraintType = value)}
+      placeholder='Type'
+    >
+      <Select.Option value='LBC'>LBC</Select.Option>
+      <Select.Option value='UBC'>UBC</Select.Option>
+    </Select>
+  );
 
   return (
     <>
       <marker
-        id="triangle"
-        markerWidth="30"
-        markerHeight="30"
-        viewBox="-20 -20 40 40"
-        orient="auto"
-        refX="0"
-        refY="0"
+        id='triangle'
+        markerWidth='30'
+        markerHeight='30'
+        viewBox='-20 -20 40 40'
+        orient='auto'
+        refX='0'
+        refY='0'
       >
         <polyline
-          stroke="#000"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="1"
-          fill="none"
-          points="-15,-13 0,0 -15,13"
+          stroke='#000'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          strokeWidth='1'
+          fill='none'
+          points='-15,-13 0,0 -15,13'
         />
       </marker>
 
       <path
         id={id}
-        className="timeConstraintEdge"
+        className='timeConstraintEdge'
         d={path}
-        markerEnd="url(#triangle)"
+        markerEnd='url(#triangle)'
         style={style}
       />
       <foreignObject
@@ -59,14 +70,15 @@ export default function TimeConstraintEdgeTop({
         height={foreignObjectSize}
         x={(sourceX + targetX) / 2 - 50}
         y={sourceY > targetY ? sourceY - 140 : targetY - 140}
-        className="edgebutton-foreignobject"
-        requiredExtensions="http://www.w3.org/1999/xhtml"
+        className='edgebutton-foreignobject'
+        requiredExtensions='http://www.w3.org/1999/xhtml'
       >
         <InputNumber
+          addonBefore={selectBefore}
           min={0}
           defaultValue={data?.label}
-          placeholder="Constraint"
-          className="timeConstraintInput"
+          placeholder='Constraint'
+          className='timeConstraintInput'
           onChange={(value) => (data.label = value)}
         />
       </foreignObject>
--- src/NodeTypes/ActivityNode.js
+++ src/NodeTypes/ActivityNode.js
@@ -1,117 +1,129 @@
-import React, { memo } from 'react';
-import { Input, InputNumber, Select } from 'antd';
-import { Handle } from 'react-flow-renderer';
+import React, { memo } from "react";
+import { Input, InputNumber, Select } from "antd";
+import { Handle } from "react-flow-renderer";
 
 export default memo(({ id, data, isConnectable }) => {
   return (
-    <div className="activityNode">
-      <div className="activityNodeLeft">
+    <div className='activityNode'>
+      <div className='activityNodeLeft'>
         <Handle
-          id={'constraintTopLeft' + id}
-          position="top"
-          style={{ background: '#000', left: '12.5%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"constraintTopLeft" + id}
+          position='top'
+          style={{ background: "#000", left: "12.5%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
         <Handle
-          id={'targetLeft' + id}
-          position="left"
-          style={{ background: '#000' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"targetLeft" + id}
+          position='left'
+          style={{ background: "#000" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Handle
-          id={'constraintBottomLeft' + id}
-          position="bottom"
-          style={{ background: '#000', left: '12.5%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"constraintBottomLeft" + id}
+          position='bottom'
+          style={{ background: "#000", left: "12.5%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
       </div>
 
-      <div className="activityNodeMiddle">
+      <div className='activityNodeMiddle'>
         <Handle
-          id={'eventTopLeft' + id}
-          position="top"
-          style={{ background: '#000', left: '35%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"eventTopLeft" + id}
+          position='top'
+          style={{ background: "#000", left: "35%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Handle
-          id={'eventTopRight' + id}
-          position="top"
-          style={{ background: '#000', left: '65%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"eventTopRight" + id}
+          position='top'
+          style={{ background: "#000", left: "65%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Input
           defaultValue={data?.name}
           onChange={(ev) => (data.name = ev.target.value)}
-          placeholder="Name"
-          className="activityInputName"
+          placeholder='Name'
+          className='activityInputName'
         />
 
-        <InputNumber
-          min={0}
-          defaultValue={data?.duration}
-          onChange={(value) => (data.duration = value)}
-          placeholder="Duration"
-          className="activityInputName"
-        />
+        <div>
+          <span className='fontSizeDuration'>[</span>
+          <InputNumber
+            min={0}
+            defaultValue={data?.durationMin}
+            onChange={(value) => (data.durationMin = value)}
+            placeholder='Duration'
+            className='activityInputDuration'
+          />
+          <span className='fontSizeDuration'>,</span>
+          <InputNumber
+            min={0}
+            defaultValue={data?.durationMax}
+            onChange={(value) => (data.durationMax = value)}
+            placeholder='Duration'
+            className='activityInputDuration'
+          />
+          <span className='fontSizeDuration'>]</span>
+        </div>
 
         <Select
           defaultValue={data?.continuousness}
           onChange={(ev) => (data.continuousness = ev)}
-          placeholder="Continuousness"
-          className="activityInputName"
+          placeholder='Continuousness'
+          className='activityInputName'
         >
-          <Select.Option value="continous">Continuous</Select.Option>
-          <Select.Option value="semiContinous">Semi-Continuous</Select.Option>
-          <Select.Option value="nonContinous">Non-Continuous</Select.Option>
+          <Select.Option value='continous'>Continuous</Select.Option>
+          <Select.Option value='semiContinous'>Semi-Continuous</Select.Option>
+          <Select.Option value='nonContinous'>Non-Continuous</Select.Option>
         </Select>
 
         <Handle
-          id={'eventBottomLeft' + id}
-          position="bottom"
-          style={{ background: '#000', left: '35%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"eventBottomLeft" + id}
+          position='bottom'
+          style={{ background: "#000", left: "35%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Handle
-          id={'eventBottomRight' + id}
-          position="bottom"
-          style={{ background: '#000', left: '65%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"eventBottomRight" + id}
+          position='bottom'
+          style={{ background: "#000", left: "65%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
       </div>
 
-      <div className="activityNodeRight">
+      <div className='activityNodeRight'>
         <Handle
-          id={'constraintTopRight' + id}
-          position="top"
-          style={{ background: '#000', left: '87.5%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"constraintTopRight" + id}
+          position='top'
+          style={{ background: "#000", left: "87.5%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Handle
-          id={'sourceRight' + id}
-          position="right"
-          style={{ background: '#000' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"sourceRight" + id}
+          position='right'
+          style={{ background: "#000" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
 
         <Handle
-          id={'constraintBottomRight' + id}
-          position="bottom"
-          style={{ background: '#000', left: '87.5%' }}
-          onConnect={(params) => console.log('handle onConnect', params)}
+          id={"constraintBottomRight" + id}
+          position='bottom'
+          style={{ background: "#000", left: "87.5%" }}
+          onConnect={(params) => console.log("handle onConnect", params)}
           isConnectable={isConnectable}
         />
       </div>
